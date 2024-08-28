0c78ffb3700b2b89ba76681505ea4eec90d42ab8
Prince Kumar <118840584+PrinceXkumar@users.noreply.github.com>
2024-08-29 00:29:41 +0530

    Add files via upload
    Part- 1 : In this I  studied about making pattern using while loop

---
+++ D6R.cpp
@@ -0,0 +1,138 @@
+
+/*
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int i=1;
+	while(i <= n){
+		int j = 1;
+		while(j <= n){
+			cout<<j<<" ";
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+}
+
+*/
+
+/*
+3 2 1
+3 2 1
+3 2 1
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int i = 1;
+	while(i <= n){
+		int j = 1;
+		while(j <= n){
+			cout<< n-j+1;
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+}
+
+*/
+
+
+/*
+123
+456
+789
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int i =1;
+	int count =1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+		cout<<count;
+		count++;
+		j++;	
+		}
+		cout<<endl;
+		i++;
+	}
+}
+
+*/
+
+
+/*
+*
+**
+***
+****
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int i =1;
+	while(i<=n){
+		int j=1;
+		while(j<=i){
+			cout<<"*";
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}	
+}
+
+*/
+
+/*
+1
+22
+333
+4444
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int i =1;
+	while(i<=n){
+		int j=1;
+		while(j<=i){
+			cout<<i;
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}	
+}
+
+*/
+
+
+
+
+
+
+
+
